2c1388598f394b63b8a15cc6cf47c38ad7b8b88a
NordineKECITA <NordineKECITA@ciage.fr>
2025-08-25 21:22:50 +0200

    test

--- Backup/Module1.vb
+++ Backup/Module1.vb
@@ -37,7 +37,7 @@ Module Module1
         Try
 
 
-            ' premier commit
+
             ' Lecture du fichier de configuration XML
             Dim i As Integer
             Dim startupPath As String
